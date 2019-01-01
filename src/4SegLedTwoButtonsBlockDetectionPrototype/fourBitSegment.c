7da1896edc1ec4503e7731038bd12d218e11ab12
Alger Pike <Alger_p@pacbell.net>
2019-01-01 09:39:51 -0800

    Removing adeept sample information since this code will now diverge into an original work.

--- src/4SegLedTwoButtonsBlockDetectionPrototype/fourBitSegment.c
+++ src/4SegLedTwoButtonsBlockDetectionPrototype/fourBitSegment.c
@@ -1,10 +1,6 @@
 /*
 * Filename    : fourBitSegment.c
 * Description : display 1,2,3,4...
-* Website     : www.adeept.com
-* E-mail      : support@adeept.com
-* Author      : Jason
-* Date        : 2015/06/21
 */
 
 #include <wiringPi.h>
